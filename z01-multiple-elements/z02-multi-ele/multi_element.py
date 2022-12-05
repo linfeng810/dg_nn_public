8cb3383198ce97a9e404cbd38b9a6e3d5d20f649
Linfeng Li <li.linfeng.xjtu@gmail.com>
2022-12-05 21:52:48 +0000

    assemble M K matrix

--- z01-multiple-elements/z02-multi-ele/multi_element.py
+++ z01-multiple-elements/z02-multi-ele/multi_element.py
@@ -314,7 +314,7 @@ class det_nlx(Module):
         x = x_loc[:,0,:].view(batch_in,1,nloc)
         y = x_loc[:,1,:].view(batch_in,1,nloc)
         # print('x',x,'\ny',y)
-        print(torch.cuda.memory_summary())
+        # print(torch.cuda.memory_summary())
         # first we calculate jacobian matrix (J^T) = [j11,j12;
         #                                             j21,j22]
         # [ d x/d xi,   dy/d xi ;
@@ -329,24 +329,18 @@ class det_nlx(Module):
         # print('j12', j12)
         # print('j21', j21)
         # print('j22', j22)
-        print(torch.cuda.memory_summary())
+        # print(torch.cuda.memory_summary())
         # calculate determinant of jacobian
         det = torch.mul(j11,j22)-torch.mul(j21,j12)
         det = det.view(batch_in, ngi)
-        print('det', det)
+        # print('det', det)
         invdet = torch.div(1.0,det)
-        print('invdet', invdet)
-
+        # print('invdet', invdet)
+        del j11, j12, j21, j22
+        ####### 
+        # calculate and store inv jacobian...
         # inverse of jacobian
-        invj11 = torch.mul(j11,invdet).view(batch_in,-1)
-        invj12 = torch.mul(j21,invdet).view(batch_in,-1)*(-1.0)
-        invj21 = torch.mul(j12,invdet).view(batch_in,-1)*(-1.0)
-        invj22 = torch.mul(j22,invdet).view(batch_in,-1)
-        # print('invj11', invj11)
-        # print('invj12', invj12)
-        # print('invj21', invj21)
-        # print('invj22', invj22)
-
+        # print(torch.cuda.memory_summary())
         # calculate nx
         # input: invjac (batch_size, ngi, ndim*ndim)
         # output: nx (batch_size, ngi, ndim, nloc)
@@ -355,22 +349,57 @@ class det_nlx(Module):
         nlx1 = nlx1.expand(batch_in,ngi,nloc)
         nlx2 = torch.tensor(np.transpose(nlx[1,:,:]), device=dev)
         nlx2 = nlx2.expand(batch_in,ngi,nloc)
-        # print('nlx1', nlx1)
-        # print('nlx2', nlx2)
+        j11 = self.calc_j11(x).view(batch_in, ngi)
+        j21 = self.calc_j21(x).view(batch_in, ngi)
+        invj11 = torch.mul(j11,invdet).view(batch_in,-1)
+        invj12 = torch.mul(j21,invdet).view(batch_in,-1)*(-1.0)
+        del j11 
+        del j21
         invj11 = invj11.unsqueeze(-1).expand(batch_in,ngi,nloc)
         invj12 = invj12.unsqueeze(-1).expand(batch_in,ngi,nloc)
-        invj21 = invj21.unsqueeze(-1).expand(batch_in,ngi,nloc)
-        invj22 = invj22.unsqueeze(-1).expand(batch_in,ngi,nloc)
-        print('invj11expand', invj22)
-        # print(invj11.shape, nlx1.shape)
         nx1 = torch.mul(invj11, nlx1) \
             + torch.mul(invj12, nlx2)
+        del invj11 
+        del invj12 
+        # print('invj11', invj11)
+        # print('invj12', invj12)
+        # print('invj21', invj21)
+        # print('invj22', invj22)
+
+        # print('nlx1', nlx1)
+        # print('nlx2', nlx2)
+        j12 = self.calc_j12(y).view(batch_in, ngi)
+        j22 = self.calc_j22(y).view(batch_in, ngi)
+        invj21 = torch.mul(j12,invdet).view(batch_in,-1)*(-1.0)
+        invj22 = torch.mul(j22,invdet).view(batch_in,-1)
+        del j12
+        del j22 
+        invj21 = invj21.unsqueeze(-1).expand(batch_in,ngi,nloc)
+        invj22 = invj22.unsqueeze(-1).expand(batch_in,ngi,nloc)
+        del invdet 
+        # print('invj11expand', invj22)
+        # print(invj11.shape, nlx1.shape)
         nx2 = torch.mul(invj21, nlx1) \
             + torch.mul(invj22, nlx2)
-        print('nx1', nx1)
-        nx = torch.stack((nx1,nx2),dim=1)
+        del invj21 
+        del invj22 
 
-        return nx, abs(det )
+        #######
+        # do not store inv jacobian but calculate on the fly!
+        # calculate nx
+        # print(torch.cuda.memory_summary())
+        # nlx1 = torch.tensor(np.transpose(nlx[0,:,:]), device=dev)
+        # nlx1 = nlx1.expand(batch_in,ngi,nloc)
+        # nlx2 = torch.tensor(np.transpose(nlx[1,:,:]), device=dev)
+        # nlx2 = nlx2.expand(batch_in,ngi,nloc)
+        # nx1 = torch.mul(torch.mul(j11,invdet).view(batch_in,-1).unsqueeze(-1).expand(batch_in,ngi,nloc), nlx1) \
+        #     - torch.mul(torch.mul(j21,invdet).view(batch_in,-1).unsqueeze(-1).expand(batch_in,ngi,nloc), nlx2)
+        # nx2 =-torch.mul(torch.mul(j12,invdet).view(batch_in,-1).unsqueeze(-1).expand(batch_in,ngi,nloc), nlx1) \
+        #     + torch.mul(torch.mul(j22,invdet).view(batch_in,-1).unsqueeze(-1).expand(batch_in,ngi,nloc), nlx2)
+        # # print('nx1', nx1)
+        nx = torch.stack((nx1,nx2),dim=1)
+        # print(torch.cuda.memory_summary())
+        return nx, abs( det )
 
 # test det_nlx shape
 # [n, nlx, weight] = SHATRInew(nloc, ngi, ndim)
@@ -429,22 +458,27 @@ x_ref_in1 = np.transpose(x_ref_in1)
 x_ref_in2 = np.transpose(x_ref_in2)
 x_ref_in = np.stack((x_ref_in1,x_ref_in2), axis=0)
 x_ref_in = torch.tensor(x_ref_in, requires_grad=False, device=dev)
+# x_ref_in = torch.transpose(x_ref_in, 0,1).unsqueeze(0)
 print('xin size', x_ref_in.shape)
-x_ref_in = x_ref_in.repeat(1000000,1,1)
-print(torch.cuda.memory_summary())
-# np.savetxt('x_ref_in.txt',x_ref_in,delimiter=',')
-# x_ref_in = torch.tensor(x_ref_in,requires_grad=False)#.unsqueeze(0)
-# print(x_ref_in.shape)
-start = time.time()
+print(x_ref_in)
+# x_ref_in = x_ref_in.repeat(1500000,1,1)
+# print(torch.cuda.memory_summary())
+# # np.savetxt('x_ref_in.txt',x_ref_in,delimiter=',')
+# # x_ref_in = torch.tensor(x_ref_in,requires_grad=False)#.unsqueeze(0)
+# # print(x_ref_in.shape)
+# start = time.time()
 with torch.no_grad():
-    output, detwei = Det_nlx.forward(x_ref_in)
-end = time.time()
-print('time: ', end-start, 'on ', dev)
-
-# # print('nx', output)
-# # print('det', detwei)
-# # np.savetxt('nx.txt', np.squeeze(output[0,0,:,:]), delimiter=',')
-# # np.savetxt('detwei.txt', detwei, delimiter=',')
+    nx, det = Det_nlx.forward(x_ref_in)
+detwei = torch.mul(det, torch.tensor(weight).unsqueeze(0).expand(det.shape[0],ngi))
+print('det size', det.shape)
+print('weight size', torch.tensor(weight).unsqueeze(0).expand(det.shape[0],ngi).shape)
+# end = time.time()
+# print('time: ', end-start, 'on ', dev)
+# print(torch.cuda.memory_summary())
+print('nx', nx)
+print('det', detwei)
+np.savetxt('nx.txt', np.squeeze(nx[0,0,:,:]), delimiter=',')
+np.savetxt('detwei.txt', detwei, delimiter=',')
 
 # # print('j11_filter', calc_j11_j12_filter)
 # # print('x_fen_in', torch.squeeze(x_ref_in[:,0,:]))
@@ -452,3 +486,75 @@ print('time: ', end-start, 'on ', dev)
 
 # test passed
 ################################################
+
+# assemble mass matrix and stiffness matrix
+# solve for intermediate c^(n*):
+# 
+# 1/dt M( c^(n*) - c^(n-1) ) + K c^(n-1) = 0
+# ==>
+# M c^(n*) = (M-dt*K) c^(n-1)
+# to form a Jacobi iteration, here we calculate 
+# M and b = (M-dt*K) c^(n-1)
+class mk(Module):
+    def __init__(self):
+        super(mk, self).__init__()
+
+    def forward(self, c, k, dt, n, nx, detwei):
+        ### input
+        # c  - node values at last timestep, (batch_size, 1, nloc)
+        # k  - diffusion coefficient at node, right now we simplify it using constant k. (1)
+        # dt - timestep. (1)
+        # n  - shape function Ni, (ngi, nloc)
+        # nx - shape function derivatives Nix & Niy, (batch_size, ndim, ngi, nloc)
+        # detwei - determinant times GI weight, (batch_size, ngi)
+        ### output
+        # nn - mass matrix (consistent), (batch_size, nloc, nloc)
+        # b  - node values at next (future) timestep, (batch_size, 1, nloc)
+
+        batch_in = c.shape[0]
+        # mass matrix
+        nn = torch.mul(n.unsqueeze(0).expand(batch_in, ngi, nloc), \
+            detwei.unsqueeze(-1).expand(batch_in, ngi, nloc))   # (batch_in, ngi, nloc)
+        nn = torch.bmm(torch.transpose(n,0,1).unsqueeze(0).expand(batch_in, nloc, ngi), \
+            nn) # (batch_in, nloc, nloc)
+        
+        # stiffness matrix 
+        nx1nx1 = torch.mul(nx[:,0,:,:].view(batch_in, ngi, nloc), \
+            detwei.unsqueeze(-1).expand(batch_in, ngi, nloc)) # (batch_in, ngi, nloc)
+        nx1nx1 = torch.bmm(torch.transpose(nx[:,0,:,:].view(batch_in, ngi, nloc), 1,2), \
+            nx1nx1) # (batch_in, nloc, nloc)
+        print('nx1nx1',nx1nx1)
+        nx2nx2 = torch.mul(nx[:,1,:,:].view(batch_in, ngi, nloc), \
+            detwei.unsqueeze(-1).expand(batch_in, ngi, nloc)) # (batch_in, ngi, nloc)
+        nx2nx2 = torch.bmm(torch.transpose(nx[:,1,:,:].view(batch_in, ngi, nloc), 1,2), \
+            nx2nx2) # (batch_in, nloc, nloc)
+        nxnx = (nx1nx1+nx2nx2)*k # scalar multiplication, (batch_in, nloc, nloc)
+        del nx1nx1 , nx2nx2 
+        print('stiffness matrix\n', nxnx)
+        nxnx = nn - nxnx*dt # this is (M-dt K), (batch_in, nloc, nloc)
+        b = torch.matmul(nxnx,torch.transpose(c,1,2)) # batch matrix-vector multiplication, 
+            # input1: (batch_in, nloc, nloc)
+            # input2: (batch_in, nloc, 1)
+            # broadcast over batch
+            # output: (batch_in, nloc, 1)
+        b = torch.transpose(b,1,2) # return to (batch_in, 1, nloc)
+        
+        return nn, b
+
+Mk = mk()
+c = np.arange(1,21).reshape(2,nloc)
+c = torch.tensor(c, dtype=torch.float64).view(-1,1,nloc)
+print(c.shape)
+print(c)
+n = torch.transpose(torch.tensor(n),0,1)
+print(type(n), n.shape)
+print(type(nx), nx.shape)
+# print('nx transpose',torch.transpose(nx[:,0,:,:].view(1, ngi, nloc), 1,2))
+print(type(detwei), detwei.shape)
+[M,b] = Mk.forward(c,1,1e-3,n,nx,detwei)
+print('mass matrix\n', torch.squeeze(M))
+# cc = torch.tensor(np.arange(1,14), dtype=torch.float64).view(1,ngi)
+# print(cc.unsqueeze(-1).expand(1, ngi, nloc))
+########################
+# Pass test!
+########################
