300be7fa531488badcf04bc21d35b7ee4f7e5907
Linfeng Li <li.linfeng.xjtu@gmail.com>
2023-01-09 09:52:46 +0000

    steady problem, two-grid, correct bc implementation via halo nodes. but not correct - error is large , there must be bugs

---
+++ z02-implicit/z03-one-node-per-element-multigrid/z01-test-steady-prob/config.py
@@ -0,0 +1,38 @@
+# configuration
+import toughio
+import numpy as np
+import torch
+
+#
+# device
+dev=torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+# dev="cpu" # if force funning on cpu
+
+#####################################################
+# time step settings
+#####################################################
+dt = 1e8 # timestep
+tstart=0 # starting time
+tend=1e8 # end time, we'll need ~2s for the modal problem to reach static state
+isTransient=False # decide if we are do transient simulation
+
+#####################################################
+# read mesh and build connectivity
+#####################################################
+filename='square.msh' # directory to mesh file (gmsh)
+mesh = toughio.read_mesh(filename) # mesh object
+
+# mesh info
+nele = mesh.n_cells # number of elements
+cubic = True # elemnet type (cubic)
+if (cubic) :
+    nloc = 10  # number of nodes in an element
+    ngi = 13 # number of quadrature points
+nonods = nloc*nele # number of nodes
+ndim = 2 # dimesnion of the problem
+ndglno=np.arange(0,nonods) # local to global 
+
+
+######################
+jac_its = 5e3 # max jacobi iteration steps
+jac_wei = 2./3.
---
+++ z02-implicit/z03-one-node-per-element-multigrid/z01-test-steady-prob/post_processing.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+
+# This is to plot 2d dg result
+# Cubic element#
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from tqdm import tqdm
+
+# input: 
+# c - field variable 1d array (nonods)
+# x_all - node coordinate, 2d array (nonods, ndim)
+
+x_data = pd.read_csv('./x_all.txt', header=None)
+x_all = x_data.to_numpy()
+print(x_all.shape)
+
+# c = np.arange(1,11)
+c_data = pd.read_csv('./c_all.txt', header=None)
+c = c_data.to_numpy()
+
+ntime = c.shape[0]
+nloc = 10
+nele = int(x_all.shape[0]/nloc )
+nonods = x_all.shape[0]
+
+# fetch final result
+c = c[1,:]
+# calculate analytical soln
+c_ana = np.zeros(nonods)
+for inod in range(nonods):
+    xi = x_all[inod,0]
+    yi = x_all[inod,1]
+    c_ana[inod] = np.sin(np.pi*xi) * np.sinh(np.pi*yi) / np.sinh(np.pi)
+# c = c - c_ana
+
+c_max = c.max() 
+c_min = c.min()
+print(c_max,c_min)
+print(c.shape)
+
+triangles = np.asarray([
+    [1,4,9],    [9,4,10],   [4,5,10], 
+    [10,5,6],   [5,2,6],    [9,10,8],
+    [8,10,7],   [7,10,6],   [8,7,3]
+])-1
+levels = np.linspace(c_min,c_max,11)
+# print(np.max(c))
+print(levels)
+
+for itime in tqdm(range(0,ntime,1)):
+    fig1, ax1 = plt.subplots()
+    for ele in range(nele):
+            
+        current_ele_idx = np.arange(ele*nloc,ele*nloc+nloc) 
+        x = x_all[current_ele_idx, 0]
+        y = x_all[current_ele_idx, 1]
+        # z = c[itime,current_ele_idx]
+        z = c[current_ele_idx]
+        # print(z)
+
+        ax1.set_aspect('equal')
+        tcf = ax1.tricontourf(x, y, z,levels=levels )#, levels=levels  )
+    fig1.colorbar(tcf)
+    # ax1.set_title('t = {0:.2f}'.format((itime)*1e-2))
+    ax1.set_title('%d elements approx'%(nele))
+    ax1.set_xlabel('x')
+    ax1.set_ylabel('y')
+
+    
+    # for ele in tqdm(range(nele)):
+            
+    #     current_ele_idx = np.arange(ele*nloc,ele*nloc+nloc) 
+    #     x = x_all[current_ele_idx, 0]
+    #     y = x_all[current_ele_idx, 1]
+    #     # z = c[itime,current_ele_idx]
+    #     z = c_ana[current_ele_idx]
+    #     # print(z)
+
+    #     ax2.set_aspect('equal')
+    #     tcf = ax2.tricontourf(x, y, z,levels=levels )#, levels=levels  )
+    # # fig2.colorbar(tcf)
+    # # ax1.set_title('t = {0:.2f}'.format((itime)*1e-2))
+    # ax2.set_title('analytical')
+    # ax2.set_xlabel('x')
+    # ax2.set_ylabel('y')
+
+    plt.savefig('diffusion{:04}.png'.format(itime))
+    plt.close()
+plt.show()
+
+# x = x_all[:, 0]
+# y = x_all[:, 1]
+# z = c[-1,:]
+# fig4, ax4 = plt.subplots()
+# ax4.set_aspect('equal')
+# tcf = ax4.tricontourf(x, y, z,levels=levels )#, levels=levels  )
+# fig4.colorbar(tcf)
+# ax4.set_title('t = {0:.2f}'.format((10000)/100))
+# ax4.set_xlabel('x')
+# ax4.set_ylabel('y')
+# plt.show()
