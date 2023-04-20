c73b61e866a401846543718550ebf486bda10342
Linfeng Li <li.linfeng.xjtu@gmail.com>
2023-04-20 22:42:24 +0100

    add to function that compute shape functions on reference element. add 3D options in config.py

--- z02-implicit/z06-DGtoCG/config.py
+++ z02-implicit/z06-DGtoCG/config.py
@@ -32,22 +32,33 @@ mesh = toughio.read_mesh(filename) # mesh object
 # mesh info
 nele = mesh.n_cells # number of elements
 ele_type = 'cubic'  # 'linear' or 'cubic'
-if ele_type=='cubic':
-    nloc = 10  # number of nodes in an element
-    ngi = 13 # number of quadrature points
-    sngi = 4 # number of surface quadrature
-    snloc = 4  # number of nodes per face
-elif ele_type=='linear':
-    nloc = 3
-    ngi = 3
-    sngi = 2
-    snloc = 2
-else:
-    raise Exception("Element type is not acceptable.")
+ndim = 3 # dimesnion of the problem
+if ndim == 2:
+    if ele_type=='cubic':
+        nloc = 10  # number of nodes in an element
+        ngi = 13 # number of quadrature points
+        sngi = 4 # number of surface quadrature
+        snloc = 4  # number of nodes per face
+    elif ele_type=='linear':
+        nloc = 3
+        ngi = 3
+        sngi = 2
+        snloc = 2
+    else:
+        raise Exception("Element type is not acceptable.")
+    p1dg_nonods = 3*nele  # number of nodes on P1DG grid
+    nface = 3 # number of element faces
+else:  # ndim = 3
+    if ele_type=='cubic':
+        nloc = 20
+        ngi = 24
+        snloc = 10
+        sngi = 9
+    else:
+        raise Exception("Element type is not acceptable.")
+    p1dg_nonods = 4*nele
+    nface = 4
 nonods = nloc*nele # number of nodes
-p1dg_nonods = 3*nele  # number of nodes on P1DG grid
-ndim = 2 # dimesnion of the problem
-nface = 3 # number of element faces
 ndglno=np.arange(0,nonods) # local to global
 cg_ndglno=[]
 cg_nonods=[]
