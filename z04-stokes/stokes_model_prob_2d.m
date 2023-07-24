ba8d73f9c011b9c1b245a3ec698f7926f52b2167
Linfeng Li <li.linfeng.xjtu@gmail.com>
2023-07-24 10:32:00 +0100

    tweak matlab script for stokes model problem bc & f calculation.
    we should use reduced stress form (mu nabla u - p I) rather than full form (mu nabla u + mu nabla^T u - pI) to calculate neumann bc. because we are use using reduced form to write weak form (maybe, im not sure. but otherwise it will not work).

--- z04-stokes/stokes_model_prob_2d.m
+++ z04-stokes/stokes_model_prob_2d.m
@@ -3,30 +3,38 @@
 % velocity should satisfy divergence-free condition
 % \nabla \cdot u = 0
 
-clear;
+% clear;
 
 syms x y z
 pi = sym(pi);
 
 u = sym(zeros(2,1));
 
-% PROBLEM Cockburn, Kanschat ...
-u(1) = -exp(x)*(y*cos(y)+sin(y));
-u(2) =exp(x)*y*sin(y);
-p = 2*exp(x)*sin(y);
+% % PROBLEM Cockburn, Kanschat ...
+% u(1) = -exp(x)*(y*cos(y)+sin(y));
+% u(2) =exp(x)*y*sin(y);
+% p = 2*exp(x)*sin(y);
+
+% PROBLEM poiseuille
+u(1) = 1 - y^2;
+u(2) = 0;
+p = -2*x+2;
 
 gradu = sym(zeros(2,2));
 for i = 1:2
-    gradu(:,i) = gradient(u(i), [x y ]);
+    gradu(i,:) = gradient(u(i), [x y ]);
 end
 gradTu = transpose(gradu);
 
 divu = divergence(u, [x y ]);
 gradp = gradient(p, [x y ]);
 
-sigma = gradu + gradTu;  % * mu (viscosity) we omit mu since mu=1
+sigma = gradu + gradTu - p*eye(2,2);
+    % * mu (viscosity) we omit mu since mu=1
 
 f = sym(zeros(2,1));
 for i = 1:2
-    f(i) = -divergence(sigma(i,:), [x y ]) + gradp(i);
+    f(i) = -divergence(sigma(i,:), [x y ]);
 end
+
+bc_neu = subs(sigma, x, 1) * [1, 0]'
--- z04-stokes/stokes_model_prob_3d.m
+++ z04-stokes/stokes_model_prob_3d.m
@@ -14,6 +14,12 @@ u(2) = sin(x)^2 * (y*cos(x) - z*sin(x));
 u(3) = sin(x)^2 * (z*cos(x) + y*sin(x));
 p = sin(x);
 
+% % PROBLEM poiseuille
+% u(1) = 1 - y^2;
+% u(2) = 0;
+% u(3) = 0;
+% p = -2*x+2;
+
 gradu = sym(zeros(3,3));
 for i = 1:3
     gradu(i,:) = gradient(u(i), [x y z]);
@@ -23,9 +29,17 @@ gradTu = transpose(gradu);
 divu = divergence(u, [x y z]);
 gradp = gradient(p, [x y z]);
 
-sigma = gradu + gradTu;  % * mu (viscosity) we omit mu since mu=1
+% sigma = gradu + gradTu - p*eye(3,3);
+    % * mu (viscosity) we omit mu since mu=1
+sigma = gradu + gradTu - p*eye(3,3);
 
 f = sym(zeros(3,1));
 for i = 1:3
-    f(i) = -divergence(sigma(i,:), [x y z]) + gradp(i);
+    f(i) = -divergence(sigma(i,:), [x y z]);
 end
+
+% neumann boundary on top surface (z=1)
+% outer normal is (0,0,1)
+bc_neu_x1 = subs(gradu-p*eye(3,3), x, 1) * [1, 0, 0]'
+bc_neu_z_1 = subs(gradu-p*eye(3,3), z, -1) * [0, 0, -1]'
+bc_neu_z1 = subs(gradu-p*eye(3,3), z, 1) * [0, 0, 1]'
