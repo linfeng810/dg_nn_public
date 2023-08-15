7eebebe8a1b58fcdc0473fc0c34654e6d5719d5f
Linfeng Li <li.linfeng.xjtu@gmail.com>
2023-08-15 14:47:36 +0100

    add Taylor vortex problem to matlab model problem script.

--- z05-ns/ns_2d_model_problem.m
+++ z05-ns/ns_2d_model_problem.m
@@ -1,15 +1,15 @@
 % this is to compute 3D NS model problem sln
 % rhs and proper bc
 
-syms x y z t Re  
+syms x y z t Re pi
 u = sym(zeros(2,1));
 
-% problem Kovasznay
-% Re = 1;
-lamb = Re/2 - sqrt(Re^2/4 + 4 * pi^2);
-u(1) = 1 - exp(lamb*x)*cos(2*pi*y);
-u(2) = lamb / 2 / pi * exp(lamb*x) * sin(2*pi*y);
-p = - 1 / 2 * exp(2*lamb*x);
+% % problem Kovasznay
+% % Re = 1;
+% lamb = Re/2 - sqrt(Re^2/4 + 4 * pi^2);
+% u(1) = 1 - exp(lamb*x)*cos(2*pi*y);
+% u(2) = lamb / 2 / pi * exp(lamb*x) * sin(2*pi*y);
+% p = - 1 / 2 * exp(2*lamb*x);
 
 % % PROBLEM from Farrell 2019
 % % rho = 1, mu = 1
@@ -25,6 +25,11 @@ p = - 1 / 2 * exp(2*lamb*x);
 % u(1) = 1 - y^2;
 % u(2) = 0;
 % p = -2*x+2;
+
+% PROBLEM Taylor vortex
+u(1) = -cos(pi*x) * sin(pi*y) * exp(-2*pi^2*t/Re);
+u(2) = sin(pi*x) * cos(pi*y) * exp(-2*pi^2*t/Re);
+p = -1/4*(cos(2*pi*x) + cos(2*pi*y)) * exp(-4*pi^2*t/Re);
 
 gradu = sym(zeros(2,2));
 for i = 1:2
@@ -49,4 +54,3 @@ end
 
 % neumann bc (x=1)
 bc_neu_x1 = subs(1/Re*gradu-p*eye(2,2), x, 1) * [1, 0]'
-bc_neu_x1_plus_adv = bc_neu_x1 - subs(u*transpose(u), x, 1) * [1, 0]'
